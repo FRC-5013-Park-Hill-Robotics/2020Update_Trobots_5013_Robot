aea9c2998b55373eb9daea0d719cc996b080794c
bmongar <bmongar@keyholesoftware.com>
2020-01-20 09:20:56 -0600

    Working Drivetrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,12 +8,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.ArcadeDrive;
-import frc.robot.subsystems.Drivetrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -96,10 +92,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    System.out.println(CommandScheduler.getInstance());
-    m_robotContainer.getTeleop().schedule();
-    System.out.println(CommandScheduler.getInstance());
-    SmartDashboard.putString("Robot Teleop","Robot Teleop called");
+
   }
 
   /**
