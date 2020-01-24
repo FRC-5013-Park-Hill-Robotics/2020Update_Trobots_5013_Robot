ad9585511b3ae460511b4a3259b9e1ef9694612c
bmongar <bmongar@keyholesoftware.com>
2020-01-24 05:29:49 -0600

    Added comments for work we need to do

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -21,17 +21,23 @@ public class Climber extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  /**Extends Climber 45 inches*/
+  public void extend(){
+    // TODO WE will need to have the encoder and determine how many pulses 45 inches is using phoenix tuner.
+  }
 
+  /**EXtends Climber fully */
   public void fullyExtend(){
-    //TODO
+    //TODO will need to determine pulses to max height, we don't want to run slack into our rope
   }
+
   public void fullyRetract(){
-    //TODO
+    //TODO Must use limit switch to prevent damage to telescope rods
   }
 
-  // negative values are down positive up, values 0...1 used as motor percent
-  // must obey limit switches
+ /**  negative values are down positive up, values 0...1 used as motor percent
+  must obey limit switches*/
   public void manualExtenstion(double throttle){
-
+    //TODO dont know if we need this or not, may want to limit throttle if we do though.
   }
 }
