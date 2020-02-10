67d340bc953e424daa3e4b2c3a3e10626a78cf27
5013-Programmer <46497490+5013-Programmer@users.noreply.github.com>
2020-02-10 15:05:31 -0600

    Liam(Jacob)
    Removed.
    edu.wpi.first.wpiutil.math.MathUtil

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpiutil.math.MathUtil;
 import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
