b38ab74148759ad911478b6ce1dbe558889ef146
bmongar <bmongar@keyholesoftware.com>
2020-02-21 04:54:46 -0600

    Commenting code

--- src/main/java/frc/robot/commands/FireAll.java
+++ src/main/java/frc/robot/commands/FireAll.java
@@ -1,9 +1,7 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/**
+ * Command Used for Auto routine, it will continue to fire until there isn't a ball in the 
+ * conveyor.  
+ */
 
 package frc.robot.commands;
 
